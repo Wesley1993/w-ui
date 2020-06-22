828f666731941bd03d832305d0ac018b13950122
吴智 <wuzhi@bunnymaicai.com>
2020-06-22 18:43:25 +0800

    0622

---
+++ src/icon/index.js
@@ -0,0 +1,17 @@
+Component({
+	externalClasses: [ 'w-class', 'w-label-class', 'w-hover-class', 'w-img-class', 'w-icon-class' ],
+	/**
+   * 组件的属性列表
+   */
+	properties: {},
+
+	/**
+   * 组件的初始数据
+   */
+	data: {},
+
+	/**
+   * 组件的方法列表
+   */
+	methods: {}
+})
